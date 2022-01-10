f5d9118dd4bb5f5cf9f75bd070072669b094a186
Ankur Gupta <ankurg052@gmail.com>
2022-01-10 13:19:38 +0530

    net

--- lib/network_crud_operation.dart
+++ lib/network_crud_operation.dart
@@ -15,7 +15,7 @@ import 'constants/colors.dart';
 class NetworkHandler with ChangeNotifier {
   // String baseurl = "http://c476-2409-4043-219f-ff15-6479-ade8-3251-54d4.ngrok.io";
   // String baseurl = "http://6171-2409-4043-304-15e7-a101-a171-bdd-cae6.ngrok.io";
-  // String baseurl = "http://localhost:3000";
+  String baseurl = "http://1535-2409-4043-2c9a-9544-8d93-7a91-55d1-35c9.ngrok.io";
   List<BookModel> bookmodels = [
      BookModel(
         title: 'Success',
@@ -31,8 +31,8 @@ class NetworkHandler with ChangeNotifier {
     return [...bookmodels];
   }
 
-  String baseurl =
-      "http://dfd3-2409-4043-2c9a-9544-2ce7-308e-e6fa-91c2.ngrok.io";
+  // String baseurl =
+      // "http://dfd3-2409-4043-2c9a-9544-2ce7-308e-e6fa-91c2.ngrok.io";
   FlutterSecureStorage storage = FlutterSecureStorage();
 
   Future get(String url) async {
